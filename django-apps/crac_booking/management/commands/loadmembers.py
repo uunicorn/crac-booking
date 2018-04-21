fb77606e258efb63e100f648081dd433d6b72311
root <root@crac.co.nz>
2018-04-21 04:33:16 +0200

    change members csv format

--- django-apps/crac_booking/management/commands/loadmembers.py
+++ django-apps/crac_booking/management/commands/loadmembers.py
@@ -1,3 +1,4 @@
+import sys
 import csv
 from crac_booking.models import *
 from django.core.management.base import BaseCommand, CommandError
@@ -8,20 +9,30 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('fname', type=str)
+        parser.add_argument('wipe', type=str)
 
     def handle(self, *args, **options):
         with open(options['fname'], 'rb') as csvfile:
             with transaction.atomic():
+		if options['wipe'] == 'true': 
+		    ImportedMember.objects.all().delete()
+		    Member.objects.all().delete()
+
                 reader = csv.DictReader(csvfile)
                 for row in reader:
                     member = Member()
-                    member.first_name = row['First Name']
-                    member.last_name = row['Last Name']
-                    member.home_phone = row['Phone']
-                    member.cell_phone = row['Alt. Phone']
-                    member.email = row['Email']
+                    member.first_name = row['FirstName']
+                    member.last_name = row['LastName']
+                    member.home_phone = row['PhoneNumber']
+                    member.cell_phone = row['MobileNumber']
+                    member.email = row['EmailAddress']
 
                     if member.first_name and member.last_name:
-                        member.save()
+                    	member.save()
 
-                        print repr(row)
+                        print repr(member)
+		
+		if Member.objects.count() < 10:
+		    sys.exit('Members file ' + options['fname'] + ' is too small, aborting.')
+
+	
